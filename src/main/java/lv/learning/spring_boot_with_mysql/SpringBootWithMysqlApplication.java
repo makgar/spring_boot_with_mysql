a340a1a47fc4a345c446ee85cbb253601d2f7974
Maksims Garipovs <maksims.garipovs@28stone.com>
2020-10-30 11:31:05 +0200

    first commit

---
+++ src/main/java/lv/learning/spring_boot_with_mysql/SpringBootWithMysqlApplication.java
@@ -0,0 +1,13 @@
+package lv.learning.spring_boot_with_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/learning/spring_boot_with_mysql/SpringBootWithMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.learning.spring_boot_with_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
